b5d149739e816204696a55a3f11fece77953e121
jiangyukun <187_6810_5877@sina.com>
2015-03-27 23:27:35 +0800

    weixin token

---
+++ src/main/java/me/jiangyu/april/web/weixin/TokenController.java
@@ -0,0 +1,18 @@
+package me.jiangyu.april.web.weixin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by jyk on 2015/3/27.
+ */
+@Controller
+@RequestMapping("/weixin")
+public class TokenController {
+    @RequestMapping("/token")
+    @ResponseBody
+    public String token(String signature, long timestamp, int nonce, String echostr) {
+        return echostr;
+    }
+}
